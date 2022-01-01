6ef6974af48ea9862de767e961ee39a908597550
builtuopn <1412723010@qq.com>
2022-01-02 01:45:13 +0800

    添加项目文件。

---
+++ RosBridgeClient/Messages/Agv/BatteryInfo.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+
+namespace RosSharp.RosBridgeClient.Messages.Agv
+{
+    /// <summary>
+    /// 电池信息
+    /// </summary>
+    public class BatteryInfo : Message
+    {
+        [JsonIgnore]
+        public const string RosMessageName = "agv_msgs/battery_info";
+
+        public short Capacity;
+        public short Voletage;
+        public short Current;
+        public short Temperature;
+        public bool BatHighcolt;
+        public bool BatLowvolt;
+        public bool BatOvercharge;
+        public bool BatOvercurrent;
+        public bool BatCharge_Overtemp;
+        public bool BatDiscgargeOvertemp;
+        public bool BatChargeLowtemp;
+        public bool BatDischargeLowtemp;
+
+        public BatteryInfo()
+        {
+            Capacity = 0; //说明：电池电量信息，0 - 100，单位：百分比
+            Voletage = 0;//说明：电池电压信息，电压，例：4868代表48.68V
+            Current = 0;//说明：电池电流信息，电流，例：-为放电+为充电 -223代表-2.33A
+            Temperature = 0;
+            BatHighcolt = false;
+            BatLowvolt = false;
+            BatOvercharge = false;
+            BatOvercharge = false;
+            BatOvercurrent = false;
+            BatCharge_Overtemp = false;
+            BatDiscgargeOvertemp = false;
+            BatChargeLowtemp = false;
+            BatDischargeLowtemp = false;
+        }
+    }
+}
---
+++ TestRos/Program.cs
@@ -0,0 +1,51 @@
+﻿using RosSharp.RosBridgeClient;
+using System;
+using Agv_msgs = RosSharp.RosBridgeClient.Messages.Agv;
+using geometry_msgs = RosSharp.RosBridgeClient.Messages.Geometry;
+namespace RosSharp.AgvTest
+{
+    public class Program
+    {
+        public class RosSocketConsoleTestProtocol
+        {
+            static readonly string uri = "ws://192.168.150.129:9090"; //Ros IP WebSocket协议 >>测试小乌龟
+
+            static void Main(string[] args)
+            {
+                RosSocket rosSocket = new RosSocket(new RosBridgeClient.Protocols.WebSocketSharpProtocol(uri));
+
+                geometry_msgs.Twist twist = new geometry_msgs.Twist();
+                 string publication_id = rosSocket.Advertise<geometry_msgs.Twist>("/turtle1/cmd_vel");
+
+                 Console.WriteLine("按下任意键开始...");
+                 Console.ReadKey(true);
+                 for (int i = 0; i < 10000000; i++)
+                 {
+                     twist.linear.x = 8f;
+                     twist.linear.y = 10f;
+                     twist.angular.z = 8f;
+                     rosSocket.Publish(publication_id, twist);
+                 }
+                 Console.WriteLine(publication_id);
+                 Console.WriteLine("按下任意键关闭...");
+                 Console.ReadKey(true);
+                 rosSocket.Unadvertise(publication_id);
+                 rosSocket.Close();
+
+                //订阅agv信息             
+                string Subscription_id1 = rosSocket.Subscribe<Agv_msgs.BatteryInfo>("battery_info", SubscriptionHandler);
+                Subscription_id1 = rosSocket.Subscribe<Agv_msgs.BatteryInfo>("battery_info", SubscriptionHandler);
+
+               
+            }
+            private static void SubscriptionHandler(Agv_msgs.BatteryInfo message)
+            {
+                Console.WriteLine(message.Capacity);
+                Console.WriteLine(message.Current);
+                Console.WriteLine(message.Voletage);
+                Console.WriteLine(message.Temperature);
+
+            }
+        }
+    }
+}
